dc195920ea86050d6920299aa625edf622aed418
Alex Nikiforov <unleash211@gmail.com>
2024-03-05 12:33:15 +0400

    first commit

---
+++ src/test/java/com/nikiforov/TestBase.java
@@ -0,0 +1,52 @@
+package com.nikiforov;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import config.WebDriverConfig;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+//        System.setProperty("driver", System.getProperty("driver", "local"));
+        WebDriverConfig driverConfig = ConfigFactory.create(WebDriverConfig.class);
+        Configuration.baseUrl = "https://store.steampowered.com/";
+        Configuration.pageLoadStrategy = "normal";
+        Configuration.timeout = 5000;
+        Configuration.browser = System.getProperty("browser", driverConfig.browserName());
+        Configuration.browserVersion = System.getProperty("browserVersion", driverConfig.browserVersion());
+        Configuration.browserSize = System.getProperty("browserSize", driverConfig.browserSize());
+        if (driverConfig.isRemote()) {
+            Configuration.remote = System.getProperty("browserRemoteUrl", driverConfig.browserRemoteUrl());
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+            Configuration.browserCapabilities = capabilities;
+        }
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        Selenide.closeWebDriver();
+    }
+}
---
+++ src/test/java/config/WebDriverConfig.java
@@ -0,0 +1,28 @@
+package config;
+
+//import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:config/${driver}.properties"
+})
+public interface WebDriverConfig extends Config {
+    @Key("browserName")
+    @DefaultValue("chrome")
+    String browserName();
+
+    @Key("browserVersion")
+    @DefaultValue("100.0")
+    String browserVersion();
+
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
+    String browserSize();
+
+    @Key("browserRemoteUrl")
+    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
+    String browserRemoteUrl();
+
+    @Key("isRemote")
+    @DefaultValue("false")
+    Boolean isRemote();
+}
